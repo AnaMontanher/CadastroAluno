6c0eea02f2cfd1c405b2e9dcde824eb480326c0d
Ana Paula Hiraiwa <hhiraiwa1@hotmail.com>
2025-05-22 23:49:19 -0300

    ajustando validações

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const alert = document.getElementById("alert");
+
+function alertOnSubmit(event) {
+  {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add("was-validated");
+  }
+  alert.classList.remove("visually-hidden");
+}
+
 (function () {
   "use strict";
   var forms = document.querySelectorAll(".needs-validation");
@@ -23,4 +37,12 @@ reset.addEventListener("click", function () {
   forms.classList.remove("was-validated");
   forms.classList.remove("is-invalid");
   forms.classList.remove("is-valid");
+  forms.classList.remove("form-control:invalid");
+  forms.classList.remove("invalid-feedback");
+  alert.classList.add("visually-hidden");
+  confSenha.classList.remove("is-valid");
+  senha.classList.remove("is-valid");
+  dtNasc.classList.remove("is-valid");
+  confEmail.classList.remove("is-valid");
+  email.classList.remove("is-valid");
 });
--- validacao.js
+++ validacao.js
@@ -26,22 +26,43 @@ tel.addEventListener("input", function (e) {
 //validar se os emails são iguais
 const email = document.getElementById("formEmail");
 const confEmail = document.getElementById("confEmail");
-confEmail.addEventListener("input", function (e) {
+confEmail.addEventListener("input", function () {
   if (email.value != confEmail.value) {
     confEmail.classList.add("is-invalid");
   } else {
+    confEmail.classList.remove("is-invalid");
     confEmail.classList.add("is-valid");
+    email.classList.add("is-valid");
   }
 });
 
 //validar se as senhas são iguais
-
 const senha = document.getElementById("formPassw");
 const confSenha = document.getElementById("confPassw");
-confSenha.addEventListener("input", function (e) {
+confSenha.addEventListener("input", function () {
   if (senha.value != confSenha.value) {
     confSenha.classList.add("is-invalid");
   } else {
+    confSenha.classList.remove("is-invalid");
     confSenha.classList.add("is-valid");
+    senha.classList.add("is-valid");
+  }
+});
+
+const dtNasc = document.getElementById("formDate");
+
+dtNasc.addEventListener("input", function (e) {
+  var value = e.target.value;
+  var dataPattern = value.replace(/$(\d{4})(\d+?$)/, "$1");
+  e.target.value = dataPattern;
+
+  let valor = dtNasc.value;
+  let vetor = valor.split("-");
+  let ano = vetor[0];
+  if (ano < 1900 || ano > Date.now()) {
+    dtNasc.classList.add("is-invalid");
+  } else {
+    dtNasc.classList.remove("is-invalid");
+    dtNasc.classList.add("is-valid");
   }
 });
